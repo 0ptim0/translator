a3d192f1c6a3a194eb7f25d0abe5040e62ae424c
Ivan Solodilov <optim000000@gmail.com>
2023-09-16 22:55:43 +0400

    Minor changes

--- translator_main.cxx
+++ translator_main.cxx
@@ -1,10 +1,12 @@
 #include <fcntl.h>
+#include <unistd.h>
 
 #include <cstdio>
+#include <nuttx/can/can.h>
 
 extern "C" int main(int argc, char **argv) {
     int fd = open("/dev/ttyS0", O_RDWR);
-    char a[] = "Translator!\n";
+    char a[] = "Translator\n";
     write(fd, a, sizeof(a));
     close(fd);
     return 0;
