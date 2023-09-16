d325e610c064761b2086814eaa1cd600912e4d2c
Ivan Solodilov <optim000000@gmail.com>
2023-09-16 16:52:01 +0400

    Build simple project

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
-#include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+// #include <zephyr/drivers/gpio.h>
+// #include <zephyr/kernel.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+// static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 int main() {
-    gpio_is_ready_dt(&led);
-    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    // gpio_is_ready_dt(&led);
+    // gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    // while (1) {
+    //     gpio_pin_toggle_dt(&led);
+    //     sleep(1);
+    // }
+    int fd = open("/dev/usart2", O_RDWR);
     while (1) {
-        gpio_pin_toggle_dt(&led);
-        printk("%s\n", CONFIG_BOARD);
-        k_msleep(1000);
+        char check[] = "Hello\n";
+        write(fd, check, sizeof(check));
+        sleep(1);
     }
     return 0;
 }
