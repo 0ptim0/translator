66cc1a39b8fadac5329b0ec5d192c102b52c19a0
Ivan Solodilov <optim000000@gmail.com>
2023-09-16 05:47:35 +0400

    Move to zephyr os

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include "core.h"
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 int main() {
-    while(1) {
+    gpio_is_ready_dt(&led);
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        printk("%s\n", CONFIG_BOARD);
+        k_msleep(1000);
     }
     return 0;
 }
